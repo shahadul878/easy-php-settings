38d42918f79c5cded6cd517a660bc8bc5402a218
Shahadul Islam <shahadul.islam1@gmail.com>
2026-02-28 23:06:37 +0600

    Prepare for deployment - workflow fixes and updates

--- class-easy-php-settings.php
+++ class-easy-php-settings.php
@@ -3,7 +3,7 @@
  * Plugin Name: Easy PHP Settings
  * Plugin URI:  https://github.com/easy-php-settings
  * Description: An easy way to manage common PHP INI settings from the WordPress admin panel.
- * Version:     1.0.6
+ * Version:     1.0.5
  * Author:      H M Shahadul Islam
  * Author URI:  https://github.com/shahadul878
  * License:     GPL-2.0+
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Contributors: shahadul878,codereyes
 Tags: php settings, ini, performance, debug, wp-config
 Requires at least: 5.0
 Tested up to: 6.9
-Stable tag: 1.0.6
+Stable tag: 1.0.5
 Requires PHP: 7.2
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
